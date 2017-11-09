ad2a3bf445aecb4a0cabbb51cf230799b1851fd6
Nick Void <mn3m00@gmail.com>
2017-11-09 13:35:18 +0300

    added global bindings for mysql

--- lesson02/terraform/b2.sh
+++ lesson02/terraform/b2.sh
@@ -4,5 +4,8 @@ main()
   echo "Started script $0 at `date` from `whoami`" 
   sudo apt-get update
   sudo apt-get install -y mysql-server
+  ## Don't use this for prod
+  sudo sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+  sudo service mysql restart
 }
 main "$@" | tee /tmp/$(basename $0)"-"$(date +"%Y%m%d_%H%M%S" )".log"
