dbbfb4b81723486d73af6113ce6adc7fcf73a0bd
Nick Void <mn3m00@gmail.com>
2017-11-09 18:42:11 +0300

    provision scripts for PHP vm and for MySQL vm were updated

--- lesson02/terraform/b1.sh
+++ lesson02/terraform/b1.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 main()
 {
-  echo "Started script $0 at `date` from `whoami`" 
+  echo "Started script $0 at `date` from `whoami` with args \"$@\"" 
+  export mysqlhost=$1
+  export mysqlusername=$2
+  export mysqlpassword=$3
   sudo apt-get update
-  sudo apt-get install -y php apache2
+  sudo apt-get install -y php apache2 subversion git php-pdo-mysql
+  sudo rm /var/www/html/index.html
+  sudo svn export --force https://github.com/mn3m0nic/terraform-exercises/trunk/lesson02/serverdata/php/site-files /var/www/html/
+  chmod 777 /var/www/html
+cat > /var/www/html/config.php << EOF
+<?php
+\$host       = "${mysqlhost}";
+\$username   = "${mysqlusername}";
+\$password   = "${mysqlpassword}";
+\$dbname     = "test"; 
+\$dsn        = "mysql:host=\$host;dbname=\$dbname"; 
+\$options    = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+?>
+EOF
+chmod 755 /var/www/html
+## DEBUG
+#env > /tmp/env
 }
 main "$@" | tee /tmp/$(basename $0)"-"$(date +"%Y%m%d_%H%M%S" )".log"
--- lesson02/terraform/b2.sh
+++ lesson02/terraform/b2.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 main()
 {
-  echo "Started script $0 at `date` from `whoami`" 
+  echo "Started script $0 at `date` from `whoami` with args \"$@\""  
+  export mysqlhost=$1
+  export mysqlusername=$2
+  export mysqlpassword=$3
   sudo apt-get update
-  sudo apt-get install -y mysql-server
+  sudo apt-get install -y mysql-server mysql-client
   ## Don't use this for prod
   sudo sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
   sudo service mysql restart
+  env | tee /tmp/env
+cat > /tmp/sqlinit.sql << EOF
+CREATE USER '${mysqlusername}'@'%' IDENTIFIED BY '${mysqlpassword}';
+GRANT ALL PRIVILEGES ON *.* TO '${mysqlusername}'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+CREATE DATABASE test;
+USE test;
+CREATE TABLE names (
+	id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+	name VARCHAR(100) NOT NULL
+);
+INSERT INTO names VALUES ('' , "first");
+INSERT INTO names VALUES ('' , "second");
+INSERT INTO names VALUES ('' , "third");
+EOF
+sudo mysql < /tmp/sqlinit.sql
 }
 main "$@" | tee /tmp/$(basename $0)"-"$(date +"%Y%m%d_%H%M%S" )".log"
